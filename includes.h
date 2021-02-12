1190a5715da7f0558a02c429d40281baec652523
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2021-02-11 21:40:32 -0800

    Create includes.h

---
+++ includes.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <iostream>
+#include <time.h>
+#include <queue>
+#include <assert.h>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+
+#define WIN32_LEAN_AND_MEAN
+#pragma comment(lib, "ws2_32.lib")
+
+#define BUFFER 1024
+
+// debugging
+using std::cout;
+using std::endl;
+using std::cin;
+using std::cerr;
+
+
+using std::string;
+using std::queue;
+using std::vector;
+using std::ostringstream;
