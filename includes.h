3df016ddedcd478e65d5d39daefbe7bc04a760c6
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2021-02-13 17:25:38 -0800

    Update includes.h

--- includes.h
+++ includes.h
@@ -1,28 +1,9 @@
 #pragma once
 #include <ws2tcpip.h>
 #include <windows.h>
-#include <iostream>
 #include <time.h>
-#include <queue>
-#include <assert.h>
-#include <fstream>
-#include <algorithm>
-#include <sstream>
-
 #define WIN32_LEAN_AND_MEAN
 #pragma comment(lib, "ws2_32.lib")
 
 #define BUFFER 1024
 #define TIMELEN 5000
-
-// debugging
-using std::cout;
-using std::endl;
-using std::cin;
-using std::cerr;
-
-
-using std::string;
-using std::queue;
-using std::vector;
-using std::ostringstream;
